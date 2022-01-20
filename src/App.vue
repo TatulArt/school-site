0df3ffea4e1d913fd456aaa79f31a5a98c668f38
TatulArt <artum405@gmail.com>
2022-01-20 23:58:18 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <SiteHeader />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SiteHeader from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SiteHeader,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="py-3 bg-primary">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 d-flex justify-content-center align-items-center">
+          <span class="text">
+            Краснодарский край, Туапсинский р-н, г. Туапсе, ул. Горького 40
+          </span>
+        </div>
+        <div class="col-md d-flex justify-content-center align-items-center">
+          <span class="text">sosh4tuapse@gmail.com</span>
+        </div>
+        <div class="col-md d-flex justify-content-center align-items-center">
+          <span class="text">2-72-42</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Школа №4</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div
+        class="collapse navbar-collapse flex-grow-0"
+        id="navbarSupportedContent"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Главная</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">О школе</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Учителя</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Новости</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Горячая линия</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.text {
+  color: rgba(255, 255, 255, 0.7);
+}
+</style>
